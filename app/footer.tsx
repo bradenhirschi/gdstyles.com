11ab5ec5bd30eb72cfddb5a8a7721ee8dd3e53fd
Braden Hirschi <bradenhirschi29@gmail.com>
2023-09-29 21:45:33 -0500

    Added hero section image and styled main text

---
+++ app/footer.tsx
@@ -0,0 +1,16 @@
+import { BiLogoChrome, BiLogoGithub } from "react-icons/bi";
+
+const Footer = () => {
+  return (
+    <footer className="py-8 border-t border-gray-100 flex gap-2 justify-center items-center">
+      <a href="https://chrome.google.com/webstore/detail/gd-styles/pjjhmajhdkfcolcpmfgimbgmkonbfnpl">
+        <BiLogoChrome className="w-[30px] h-[30px] text-secondary" />
+      </a>
+      <a href="https://github.com/bradenhirschi/gd-styles">
+        <BiLogoGithub className="w-[30px] h-[30px] text-secondary" />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
-import Navbar from "./navbar";
+import Image from "next/image";
+import screenshot from "../public/screenshot.png";
 
 const GdStyles = () => {
   return (
     <main>
-      <Navbar />
-      <section className="grid grid-cols-2 px-4 md:px-16 lg:px-40 py-8">
-        <div>
-          <h1>“Good designers copy, great designers steal.”</h1>
-          <h2>- Pablo Picasso</h2>
+      <section className="py-24 flex flex-col items-center">
+        <h1 className="text-5xl font-bold">Good designers copy,</h1>
+        <h1 className="text-5xl pb-2 pl-60 font-bold bg-clip-text bg-gradient-to-br from-blue-900 to-green-200 text-transparent">
+          Great designers steal
+        </h1>
+        <h4 className="mt-2">- Pablo Picasso</h4>
+        <div className="mx-44 mt-4">
+          <Image
+            src={screenshot}
+            alt="screenshot of GD Styles in action"
+            className="rounded-2xl"
+          />
         </div>
       </section>
     </main>
