d9378e6c465765f8c9b5e0b06abf042b05c251d2
Braden Hirschi <bradenhirschi29@gmail.com>
2023-09-29 21:56:59 -0500

    Fixed rendering issue and adjusted text spacing

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ const GdStyles = () => {
   return (
     <main>
       <section className="py-24 flex flex-col items-center">
-        <h1 className="text-5xl font-bold">Good designers copy,</h1>
-        <h1 className="text-5xl pb-2 pl-60 font-bold bg-clip-text bg-gradient-to-br from-blue-900 to-green-200 text-transparent">
+        <h1 className="text-5xl font-bold mr-44">Good designers copy,</h1>
+        <h1 className="text-5xl pb-2 ml-44 font-bold bg-clip-text bg-gradient-to-br from-blue-900 to-green-200 text-transparent">
           Great designers steal
         </h1>
-        <h4 className="mt-2">- Pablo Picasso</h4>
-        <div className="mx-44 mt-4">
+        <h4 className="text-gray-800">- Pablo Picasso</h4>
+        <div className="mx-44 mt-12">
           <Image
             src={screenshot}
             alt="screenshot of GD Styles in action"
